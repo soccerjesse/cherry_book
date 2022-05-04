d514ec2b9da936506b3f206ed8b1cbee80c2c092
soccerjesse <jishperu10@gmail.com>
2022-05-04 23:32:48 +0900

    hash and symbol

--- lesson_one.rb
+++ lesson_one.rb
@@ -5,3 +5,19 @@ puts a
 
 b = "こんにちは"
 puts b
+
+
+def find(country)
+  c = { jp: "yen", us: "dor" }
+  c[country]
+end
+
+def show(country)
+  ss = find(country)
+  if ss ||= 'hello'
+    ss&.upcase
+  end
+end
+
+puts show(:jp)
+puts show(:usa)
---
+++ lib/convert_length.rb
@@ -0,0 +1,4 @@
+UNIT = { m: 1, ft: 3.28, in: 39.37 }
+def convert_length(length, from: :m, to: :m)
+  (length / UNIT[from] * UNIT[to]).round(2)
+end
